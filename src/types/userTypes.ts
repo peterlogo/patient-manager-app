64c289ec731be3b9f2068564d1c0ee9fe5d06166
Peter Logo <peterlogo77@gmail.com>
2023-04-14 13:24:10 -0400

    User model implemented

---
+++ src/types/userTypes.ts
@@ -0,0 +1,19 @@
+/**
+ * User data type definition.
+ */
+export interface User {
+  firstName: string;
+  lastName: string;
+  avatar?: string;
+  email: string;
+  password: string;
+  role?: UserRole;
+}
+
+/**
+ * User role type definition.
+ */
+export enum UserRole {
+  ADMIN,
+  USER
+}
