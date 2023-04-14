0424f6ea8dbf9cb2fef3d60d6f31f0f1977c02e2
Peter Logo <peterlogo77@gmail.com>
2023-04-14 12:17:25 -0400

    Mongoose connection implemented

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import { pinoHttp } from 'pino-http';
 import { logger } from './services';
 import { config } from './config';
+import { databaseLogger, initializeMongoDBConnection } from './db';
 
 const { port } = config;
 const app: Express = express();
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(pinoHttp({ logger }));
+
+initializeMongoDBConnection().catch((err) => {
+  databaseLogger.error('Error initializing MongoDB connection', err);
+});
+
+mongoose.connection.on('connected', () => {
+  databaseLogger.info('Connected to MongoDB');
+});
+mongoose.connection.on('disconnected', () => {
+  databaseLogger.error('Disconnected from MongoDB');
+});
+mongoose.connection.on('error', (err) => {
+  databaseLogger.error('MongoDB connection error', err);
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.on('close', () => {
+    logger.info('Database connection closed');
+    process.exit(0);
+  });
+});
 
 app.listen(port, () => logger.info(`Server listening on port ${port}`));
