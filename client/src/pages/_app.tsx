78a5334d09f0b55258cd59581be833364317149f
Peter Logo <peterlogo77@gmail.com>
2023-04-20 09:31:50 -0400

    Updated client development env

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Patient Manager</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider>
+        <ModalsProvider>
+          <Notifications />
+          <Component {...pageProps} />
+        </ModalsProvider>
+      </MantineProvider>
+    </>
+  );
 }
